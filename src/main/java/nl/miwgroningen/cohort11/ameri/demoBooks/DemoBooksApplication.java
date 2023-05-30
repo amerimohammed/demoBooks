af8323763b5cb9056e459d8d61a2c4c223a91f0f
MohammedAlameri <ma@MacBook-Pro-van-MA.local>
2023-05-30 15:36:22 +0200

    initial commit

---
+++ src/main/java/nl/miwgroningen/cohort11/ameri/demoBooks/DemoBooksApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.cohort11.ameri.demoBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBooksApplication.class, args);
+	}
+
+}
